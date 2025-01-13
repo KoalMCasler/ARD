1b3fb21f2bae5fb68fc42ef70a881f9904f48ba2
KoalMCasler <Theeoblivion@gmail.com>
2025-01-13 17:32:46 -0400

    Finished tutorial

---
+++ Assets/_ARPortal/Scripts/ARPortal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPortal : MonoBehaviour
+{
+    public GameObject infoTextObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        infoTextObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
